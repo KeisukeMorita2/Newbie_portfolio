06279f70b6906f3288ec8d8dbcfa8b261f63abd6
keisuke.morita <selftracker1101@gmail.com>
2020-02-16 13:09:05 +0000

    フォロー機能・お気に入り機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
+  get 'relationships/create'
+  get 'relationships/destroy'
   root to: 'toppages#index'
   resources :users, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
     member do
